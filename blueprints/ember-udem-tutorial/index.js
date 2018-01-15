d704941ad169173799ff6f0d31706597c475506f
Alberto Cantú Gómez <betocantu93@gmail.com>
2018-01-14 19:10:37 -0600

    finished one command installation

--- blueprints/ember-udem-tutorial/index.js
+++ blueprints/ember-udem-tutorial/index.js
@@ -3,15 +3,36 @@ module.exports = {
   name: 'ember-udem-tutorial',
   normalizeEntityName() {
   },
-  afterInstall: function() {
+  afterInstall: function(options) {
 
-    return this.addPackageToProject('ember-twitch-normalizer').then(() => {
-      return this.addPackageToProject("ember-composable-helpers").then(()=> {
-        return this.addPackageToProject("ember-truth-helpers").then(()=>{
-          return this.addPackageToProject("ember-math-helpers");
-        }
-      })
-    });
+    let ui = this.ui;
+
+    let blueprintTask = this.taskFor('generate-from-blueprint');
+
+    var options = {
+      args: ['ember-twitch-normalizer', 'latest'],
+      dryRun: false,
+      verbose: false,
+      disableAnalytics: false
+    };
+    ui.writeLine('  Including Twitch Normalizer For Ember');
+
+    return blueprintTask.run(options).then(() => {
+        ui.writeLine("  Including Ember Composable Helpers");
+        return this.addPackageToProject("ember-composable-helpers", "latest");
+      }).then(() => {
+        ui.writeLine("  Including Ember Math Helpers");
+        return this.addPackageToProject("ember-math-helpers", "latest");
+      }).then(()=>{
+        ui.writeLine("  Including Ember Truth Helpers");
+        return this.addPackageToProject("ember-truth-helpers",  "latest");
+      }).then(()=> {
+        ui.writeLine("  Including Ember Paper");
+        options.args = ['ember-paper',  "latest"];
+        return blueprintTask.run(options);
+      });
+
+
 
   }
 };
